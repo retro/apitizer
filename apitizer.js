98d2ba8006a34e2dee902688b3b52ee1bb3bee94
Mihael Konjevic <konjevic@gmail.com>
2014-05-04 14:57:30 +0200

    Start working on the types

---
+++ apitizer.js
@@ -0,0 +1,12 @@
+requirejs(['can/construct', 'can/util/fixture', 'tv4', './lib/types'], function(Construct, fixture, tv4, types){
+
+	var Apitizer = Construct.extend({
+		init : function(){
+			
+		}
+	})
+
+	console.log(types)
+
+	return Apitizer;
+})
---
+++ requirejsconfig.js
@@ -0,0 +1,16 @@
+requirejs.config({
+  paths: {
+    can      : 'bower_components/canjs/amd/can',
+    jquery   : 'bower_components/jquery/dist/jquery',
+    mustache : 'bower_components/require-can-renderers/lib/mustache',
+    ejs      : 'bower_components/require-can-renderers/lib/ejs',
+    faker    : 'bower_components/Faker.js/Faker',
+    lodash   : 'bower_components/lodash/dist/lodash',
+    tv4      : 'bower_components/tv4/tv4'
+  },
+  shim : {
+    faker : {
+      exports : 'Faker'
+    }
+  }
+});
