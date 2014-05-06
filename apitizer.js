2957648cf9452bdc4d0d4cf4e03c3c00279e83fa
Mihael Konjevic <konjevic@gmail.com>
2014-05-07 00:40:59 +0200

    Work on the apitizer API and add array type

--- apitizer.js
+++ apitizer.js
@@ -9,48 +9,82 @@ requirejs([
 	var currentPath = [];
 
 	var schemaGenerators = {
-		"array"   : function(name, schema){},
-		"boolean" : function(name, schema){
+		"array"   : function(schema){
+			var items = schema.items,
+				generators = _.map(items, function(item){
+					 determineGenerator(item)(item).generate
+				});
+			if(_.isArray(items)){
+				return {
+					generate : function(){
+						return _.map(generators, function(generator){
+							return generator();
+						});
+					}
+				}
+			} else {
+				return types.array(_.merge({
+					generator : determineGenerator(schema.items)(schema.items).generate
+				}, schema));
+			}
+		},
+		"boolean" : function(schema){
 			return types.boolean(schema);
 		},
-		"integer" : function(name, schema){
+		"integer" : function(schema){
 			return types.integer(schema);
 		},
-		"number"  : function(name, schema){
+		"number"  : function(schema){
 			return types.number(schema);
 		},
-		"null"    : function(name, schema){
+		"null"    : function(schema){
 			return types.null(schema);
 		},
-		"object"  : function(name, schema){
+		"object"  : function(schema){
 			return {
 				generate : function(){
 					return _.reduce(schema.properties, function(generators, prop, propName){
-						generators[propName] = determineGenerator(prop)(propName, prop);
+						generators[propName] = determineGenerator(prop)(prop);
 						return generators;
 					}, {});
 				}
 			}
 		},
-		"string"  : function(name, schema){
+		"string"  : function(schema){
 			if(schema.pattern){
 				return types.randexp(schema);
 			} else if(schema.format){
 				return types[schema.format](schema);
 			}
 			return types.string(schema);
+		},
+		"enum"   : function(schema){
+			var values = schema.enum,
+				type   = schema.type;
+			if(type){
+				values = _.reduce(values, function(acc, current){
+					(typeof current === type) && acc.push(current);
+					return acc;
+				}, []);
+			}
+			return {
+				generate : function(){
+					var rand = types.integer({minimum: 0, maximum: values.length, exclusiveMaximum: true});
+					return values[rand.generate()];
+				}
+			}
 		}
 	}
 
 	var combinatorGenerators = {
-		anyOf : function(name, schema){
+		anyOf : function(schema){
 			var clonedSchema = _.clone(schema),
 				generators;
 
 			delete clonedSchema.anyOf;
 
 			generators = _.map(schema.anyOf, function(subschema){
-				return determineGenerator(_.extend({}, clonedSchema, subschema))(name, schema);
+				return determineGenerator(_.extend({}, clonedSchema, subschema))(schema);
 			});
 
 			return {
@@ -61,37 +95,55 @@ requirejs([
 								exclusiveMaximum: true
 							}).generate();
 
-					return generators[i]();
+					return generators[i].generate();
 				}
 			}
 		},
-		allOf : function(name, schema){
+		allOf : function(schema){
 			return {
-				generate : determineGenerator(_.merge.apply(_, schema.allOf))(name, schema)
+				generate : determineGenerator(_.merge.apply(_, schema.allOf))(schema).generate
 			};
 		},
-		oneOf : function(name, schema){
+		oneOf : function(schema){
 			var generator = combinatorGenerators.anyOf()
 			return {
 				generate : function(){}
 			}
 		},
-		not : function(name, schema){
+		not : function(schema){
 			return {
 				generate : function(){}
 			}
 		}
 	}
 
-	var refSchema = function(ref){
-		return {
-			generate : function(){}
+	var refSchema = function(schema){
+		var refPath = schema["$ref"].split('#'),
+			ref = refPath.pop().replace(/^\//, "").replace(/\/$/, ""),
+			schemaName = refPath.length > 0 && refPath[0] !== "" ? refPath.shift() : currentSchema;
+		
+		delete schema["$ref"];
+
+		return function(s){
+			return {
+				generate : function(){
+					var refSchema = getRef(rawSchemas[schemaName], ref.split('/'));
+					return determineGenerator(_.merge(schema, refSchema))(_.merge(schema, refSchema)).generate();
+				}
+			}
 		}
+	}
+
+	var getRef = function(schema, r){
+		if(r.length){
+			return getRef(schema[r.shift()], r);
+		}
+		return schema;
 	}
 
 	var determineGenerator = function(schema){
 		if(schema['$ref']){
-			return determineGenerator(refSchema(schema['$ref']));
+			return refSchema(_.clone(schema));
 		} else if(schema.anyOf){
 			return combinatorGenerators.anyOf;
 		} else if(schema.allOf){
@@ -100,6 +152,8 @@ requirejs([
 			return combinatorGenerators.oneOf;
 		} else if(schema.not){
 			return combinatorGenerators.not
+		} else if(schema.enum){
+			return schemaGenerators.enum;
 		} else if(schema.type){
 			return schemaGenerators[schema.type];
 		} else if(schema.format){
@@ -121,109 +175,73 @@ requirejs([
 		}
 	}
 
-	var withName = function(name, fn){
-		currentPath.push(name);
-		schemas[currentPath.join('.')] = fn();
-		currentPath.pop();
-	}
-
 	var schemas = {};
-
-	var makeSchemaGenerators = function(name, schema){
-		var generator
-	}
-
+	var rawSchemas = {};
 	var generators = {};
-
+	var currentSchema = "";
 
 	var apitizer = {
 		addSchema : function(name, schema){
-			schemas[name] = determineGenerator(schema)(name, schema).generate();
+			currentSchema = name;
+			schemas[name] = determineGenerator(schema)(schema).generate();
+			tv4.addSchema(name, schema);
+			rawSchemas[name] = schema;
+			currentSchema = "";
 		},
 		generateFromSchema : function(name){
 			return _.merge({}, schemas[name], function merger(current, generator){
-				var val = generator.generate();
+				var val = _.isFunction(generator.generate) ? generator.generate() : generate;
 				return _.isPlainObject(val) ?  _.merge({}, val, merger) : val;
 			})
+		},
+		validateWithSchema : function(name, data){
+			return tv4.validateMultiple(data, name);
+		},
+		addFormat : function(){
+
 		}
 	};
 
-	apitizer.addSchema('address', {
-  "type": "object",
-  "properties": {
-    "street_address": { "type": "string" },
-    "city":           { "type": "string" },
-    "state":          { "type": "string" },
-    "uuid" : {"type" : "string", pattern : /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/},
-    "foo" : {
-    	type : "object",
-    	properties : {
-    		bar : {type: "integer"}
-    	}
+apitizer.addSchema('address', {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+
+  "definitions": {
+    "address": {
+      "type": "object",
+      "properties": {
+        "street_address": { "type": "string" },
+        "city":           { "type": "string" },
+        "state":          { "type": "string" },
+        "phoneNumbers" : {"type": "array", items: {
+        	type: "integer",
+        	minimum : 1000000,
+        	maximum : 9999999
+        }}
+      },
+      "required": ["street_address", "city", "state"]
     }
   },
-  "required": ["street_address", "city", "state"]
+
+  "type": "object",
+
+  "properties": {
+    "billing_address": { "$ref": "#/definitions/address" },
+    "shipping_address": {
+      "allOf": [
+        { "$ref": "#/definitions/address" },
+        { "properties":
+          { "type": { "enum": [ "residential", "business" ] } },
+          "required": ["type"]
+        }
+      ]
+    }
+  }
 })
 
-console.log(schemas)
+//console.log(schemas)
 
-console.log(apitizer.generateFromSchema('address'))
+console.log(apitizer.validateWithSchema(apitizer.generateFromSchema('address')), apitizer.generateFromSchema('address'))
 
-
-	/*var Apitizer = Construct.extend({
-		init : function(){
-			this.schemas = {};
-			this.formats = {};
-		},
-		addSchema : function(name, schema){
-			this.schemas[name] = schema;
-			if(schema.properties){
-				tv4.addSchema(name, schema);
-			} else {
-				tv4.addSchema(name, {
-					title : name + " schema",
-					type : 'object',
-					properties : schema
-				});
-			}
-		},
-		generateFromSchema : function(name){
-			var schema = this.schemas[name],
-				self   = this;
-			return _.reduce(schema, function(res, val, key){
-
-				if(val['$ref']){
-					res[key] = self.generateFromSchema(val['$ref']);
-				} else {
-					res[key] = self.formats[val['format'] || val['type']].generate();
-				}
-
-				return res;
-			}, {});
-		},
-		validate : function(schema, data){
-			return tv4.validateMultiple(data, schema);
-		},
-		addFormat : function(name, fn){
-			var type = fn();
-			tv4.addFormat(name, function(val){
-				if(!type.validate(val)){
-					return "is not a valid " + name + " with options: " + JSON.stringify(type.opts);
-				}
-				return null;
-			});
-			this.formats[name] = type;
-		}
-
-	})
-
-	var apitizer = new Apitizer();
-
-	_.each(types, function(fn, name){
-		if(name !== 'arrayOf'){
-			apitizer.addFormat(name, fn);
-		}
-	})*/
 
 	window.apitizer = apitizer;
 
